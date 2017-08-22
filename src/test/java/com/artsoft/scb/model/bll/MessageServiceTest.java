3288a17c87f698bb5b31939e0ef0ca03934f56f5
Ian Sarasty Medina <i.sarasty@uniandes.edu.co>
2017-08-21 23:31:47 -0500

    Se comenta el codigo de la prueba de envio de correo para no estar recibiendo correos a cada rato xD

--- src/test/java/com/artsoft/scb/model/bll/MessageServiceTest.java
+++ src/test/java/com/artsoft/scb/model/bll/MessageServiceTest.java
@@ -13,44 +13,44 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 public class MessageServiceTest {
 	
-	@TestConfiguration
-    static class MessageServiceImplTestContextConfiguration {
-  
-        @Bean
-        public MessageService messageService() {
-            return new MessageService();
-        }
-    }
-	
-	@Autowired
-	private MessageService ms; 
-	
-	@Before
-    public void setUp() {
-        
-    }
-	
-	@Test
-	public void sentMessageTest() throws Exception {
-		
-		String msgHtml = "<h1>Test Mails</h1>"
-				+ "<p>La peor forma de extrañar a alguien es estar sentado a su lado y saber que nunca lo podrás tener."
-				+ "La memoria del corazón elimina los malos recuerdos y magnifica los buenos, y gracias a ese artificio, logramos sobrellevar el pasado.";
-		
-		String msg = "This email was sent with AWS";
-		String subject = "unitTest";
-		
-		List<String> listMails = new ArrayList<String>();
-		
-		listMails.add("dknieto.06@gmail.com");
-		listMails.add("darthian007@gmail.com");
-		listMails.add("iansm@hotmail.com");
-		
-		Boolean flag = ms.sendMessage(msgHtml, msg, listMails, subject);
-		assertTrue(flag);
-	} 
+//	@TestConfiguration
+//    static class MessageServiceImplTestContextConfiguration {
+//  
+//        @Bean
+//        public MessageService messageService() {
+//            return new MessageService();
+//        }
+//    }
+//	
+//	@Autowired
+//	private MessageService ms; 
+//	
+//	@Before
+//    public void setUp() {
+//        
+//    }
+//	
+////	@Test
+//	public void sentMessageTest() throws Exception {
+//		
+//		String msgHtml = "<h1>Test Mails</h1>"
+//				+ "<p>La peor forma de extrañar a alguien es estar sentado a su lado y saber que nunca lo podrás tener."
+//				+ "La memoria del corazón elimina los malos recuerdos y magnifica los buenos, y gracias a ese artificio, logramos sobrellevar el pasado.";
+//		
+//		String msg = "This email was sent with AWS";
+//		String subject = "unitTest";
+//		
+//		List<String> listMails = new ArrayList<String>();
+//		
+//		listMails.add("dknieto.06@gmail.com");
+//		listMails.add("darthian007@gmail.com");
+//		listMails.add("iansm@hotmail.com");
+//		
+//		Boolean flag = ms.sendMessage(msgHtml, msg, listMails, subject);
+//		assertTrue(flag);
+//	} 
 
 }
