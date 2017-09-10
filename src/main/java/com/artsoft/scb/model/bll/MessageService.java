42b55321bca921439bb9ecc7cbc9c78db4622b75
Ian Sarasty Medina <i.sarasty@uniandes.edu.co>
2017-09-09 21:15:14 -0500

    se crea codigo para parametrizacion de llaves de AWS desde BD

--- src/main/java/com/artsoft/scb/model/bll/MessageService.java
+++ src/main/java/com/artsoft/scb/model/bll/MessageService.java
@@ -5,9 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.simpleemail.*;
 import com.amazonaws.services.simpleemail.model.*;
 import com.artsoft.scb.model.bll.interfaces.IMessageService;
+import com.artsoft.scb.model.entity.AWScredential;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.*;
 
 @Service
@@ -15,29 +22,34 @@ public class MessageService extends ExceptionService implements IMessageService 
 
 	@Autowired
 	private HelperService helperService;
-	
+
+	@Autowired
+	private AWScredentialService awsCredentialService;
+
 	// Replace sender@example.com with your "From" address.
 	// This address must be verified with Amazon SES.
 	static final String FROM = "user@goidea.com.co";
 
 	// Replace recipient@example.com with a "To" address. If your account
 	// is still in the sandbox, this address must be verified.
-//	static final String TO = "dknieto.06@gmail.com";
+	// static final String TO = "dknieto.06@gmail.com";
 
 	// The configuration set to use for this email. If you do not want to use a
 	// configuration set, comment the next line and line 60.
-//	static final String CONFIGSET = "ConfigSet";
+	// static final String CONFIGSET = "ConfigSet";
 
 	// The subject line for the email.
-//	static final String SUBJECT = "Ich liebe dich";
+	// static final String SUBJECT = "Ich liebe dich";
 
 	// The HTML body for the email.
-//	static final String HTMLBODY = "<h1>Ich liebe dich</h1>"
-//			+ "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
-//			+ "Amazon SES</a> using the <a href='https://aws.amazon.com/sdk-for-java/'>" + "AWS SDK for Java</a>";
+	// static final String HTMLBODY = "<h1>Ich liebe dich</h1>"
+	// + "<p>This email was sent with <a href='https://aws.amazon.com/ses/'>"
+	// + "Amazon SES</a> using the <a
+	// href='https://aws.amazon.com/sdk-for-java/'>" + "AWS SDK for Java</a>";
 
 	// The email body for recipients with non-HTML email clients.
-//	static final String TEXTBODY = "This email was sent through Amazon SES " + "using the AWS SDK for Java.";
+	// static final String TEXTBODY = "This email was sent through Amazon SES "
+	// + "using the AWS SDK for Java.";
 
 	// The subject line for the email.
 
@@ -45,8 +57,18 @@ public class MessageService extends ExceptionService implements IMessageService 
 	public boolean sendMessage(String htmlBody, List<String> destinies, String subject) throws Exception {
 
 		try {
-			String textBody = helperService.RemoveHtmlTags(htmlBody);			
-			AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard()
+
+			List<AWScredential> credentialList = awsCredentialService.getCredentials();
+			AWSCredentials cred=null;
+			if (credentialList != null && credentialList.size() > 0) {
+				cred = new BasicAWSCredentials(credentialList.get(0).getKey(), credentialList.get(0).getValue());
+				AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+						.withCredentials(new AWSStaticCredentialsProvider(cred)).build();
+			}
+
+			String textBody = helperService.RemoveHtmlTags(htmlBody);
+			AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard().withCredentials((AWSCredentialsProvider) cred)
+
 					// Replace US_WEST_2 with the AWS Region you're using for
 					// Amazon SES.
 					.withRegion(Regions.US_EAST_1).build();
@@ -58,14 +80,14 @@ public class MessageService extends ExceptionService implements IMessageService 
 									.withText(new Content().withCharset("UTF-8").withData(textBody)))
 							.withSubject(new Content().withCharset("UTF-8").withData(subject)))
 					.withSource(FROM);
-					// Comment or remove the next line if you are not using a
-					// configuration set
-//					.withConfigurationSetName(CONFIGSET);
+			// Comment or remove the next line if you are not using a
+			// configuration set
+			// .withConfigurationSetName(CONFIGSET);
 			client.sendEmail(request);
-			System.out.println("Email sent!");			
-		} catch (Exception ex) {			
+			System.out.println("Email sent!");
+		} catch (Exception ex) {
 			System.out.println("The email was not sent. Error message: " + ex.getMessage());
-			throwException("Response","The email was not sent. Error message: " + ex.getMessage());
+			throwException("Response", "The email was not sent. Error message: " + ex.getMessage());
 		}
 		return true;
 	}
---
+++ src/main/java/com/artsoft/scb/model/dao/AWScredentialRepository.java
@@ -0,0 +1,9 @@
+package com.artsoft.scb.model.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.artsoft.scb.model.entity.AWScredential;
+
+public interface AWScredentialRepository extends CrudRepository<AWScredential, Integer> {
+
+}
