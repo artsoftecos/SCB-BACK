38f399cf230c71f638ca7618e1fb4ecb9718b804
Walter ALonso <wj.alonso@uniandes.edu.co>
2017-10-01 20:33:04 -0500

    se agrega una controladorapara convocatoria

---
+++ src/main/java/com/artsoft/scb/controller/ConvocatoryController.java
@@ -0,0 +1,33 @@
+package com.artsoft.scb.controller;
+
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@PreAuthorize("hasRole('ROLE_OFFERER')")
+@RequestMapping(path = "/convocatory")
+public class ConvocatoryController {
+	
+	/**
+	 * crea la convocatoria
+	 */
+	@PostMapping()
+	public ResponseEntity<?> post() {		
+		JSONObject response = new JSONObject();
+		try {			
+			//creara la convocatoria
+			response.put("Response", "Convocatoria Creada");
+		}
+		catch(Exception ex){
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());			
+		}
+		return ResponseEntity.status(HttpStatus.OK).body(response.toString());
+	}
+	
+}
+
--- src/main/java/com/artsoft/scb/controller/ConvocatoryTypeController.java
+++ src/main/java/com/artsoft/scb/controller/ConvocatoryTypeController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,7 @@ import com.artsoft.scb.model.bll.ConvocatoryTypeService;
 import com.artsoft.scb.model.entity.ConvocatoryType;
 
 @RestController
-@PreAuthorize("hasRole('ROLE_OFFERER')")
+//@PreAuthorize("hasRole('ROLE_OFFERER')")
 @RequestMapping(path = "/convocatoryType")
 public class ConvocatoryTypeController {
 
