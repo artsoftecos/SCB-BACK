3d0f60028bb134e23e46dd534801cfb5b3a7c6f4
Walter ALonso <wj.alonso@uniandes.edu.co>
2017-10-01 19:59:44 -0500

    se agrega a convocatory type seguridad

--- src/main/java/com/artsoft/scb/controller/ConvocatoryTypeController.java
+++ src/main/java/com/artsoft/scb/controller/ConvocatoryTypeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ import com.artsoft.scb.model.bll.ConvocatoryTypeService;
 import com.artsoft.scb.model.entity.ConvocatoryType;
 
 @RestController
-@Secured(value = { "ROLE_OFFERER" }) 
+@PreAuthorize("hasRole('ROLE_OFFERER')")
 @RequestMapping(path = "/convocatoryType")
 public class ConvocatoryTypeController {
 
