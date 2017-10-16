6d6deaafe9bfa594b56ab3105fd6f76200ad9739
Ian Sarasty Medina <i.sarasty@uniandes.edu.co>
2017-10-16 15:50:44 -0500

    creacion servicio de campo

---
+++ src/main/java/com/artsoft/scb/controller/FieldController.java
@@ -0,0 +1,46 @@
+package com.artsoft.scb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.artsoft.scb.model.bll.FieldService;
+import com.artsoft.scb.model.bll.interfaces.IFieldService;
+import com.artsoft.scb.model.entity.Field;
+
+@RestController
+@RequestMapping(path = "/field")
+public class FieldController {
+	
+	@Autowired
+	private IFieldService fieldService;
+	
+	@PostMapping(path = "/create")
+	public ResponseEntity<?> createField(@RequestBody Field field) {
+		
+		try {
+			fieldService.createField(field);
+		} catch (Exception ex) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body("Field Created!");
+	}
+	
+	@PostMapping(path = "/delete")
+	public ResponseEntity<?> deleteField(@RequestBody Field field) {
+		
+		try {
+			fieldService.deleteField(String.valueOf(field.getId()));
+		} catch (Exception ex) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body("Field Deleted!");
+	}
+
+}
---
+++ src/main/java/com/artsoft/scb/model/dao/FieldRespository.java
@@ -0,0 +1,13 @@
+package com.artsoft.scb.model.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.artsoft.scb.model.entity.Field;
+
+public interface FieldRespository extends CrudRepository<Field, String> {
+	
+	public List<Field> findByidPhase(int idPhase);
+	
+}
